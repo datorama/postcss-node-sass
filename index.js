b17ea3db4abeb9c8db64ee8612436d6fbea3cef1
Amit BH <amit@datorama.com>
2020-03-31 15:36:35 +0300

    fix(plugin) - properly support async plugin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let postcss = require('postcss'),
     sass = require('node-sass'),
     mergeSourceMap = require('merge-source-map');
 
-module.exports = postcss.plugin('postcss-node-sass', opt => (root, result) => {
+module.exports = postcss.plugin('postcss-node-sass', (opt) => (root, result) => {
     let css = root.toResult(Object.assign(result.opts, {
         map: { annotation: false, inline: false, sourcesContent: true }
     }));
@@ -22,15 +22,25 @@ module.exports = postcss.plugin('postcss-node-sass', opt => (root, result) => {
         file: result.opts.from,
         outFile: result.opts.to
     });
-    return new Promise((resolve, reject) => sass.render(
-        opt,
-        (err, res) => err ? console.log(err) && reject(err) : console.log('rendering sass') && resolve(res)
-    )).then(res => postcss.parse(res.css.toString(), {
-        from: result.opts.from,
-        map: {
-            prev: mergeSourceMap(css.map.toJSON(), JSON.parse(css.map))
-        }
-    })).then(res => {
-        result.root = res;
+
+    return new Promise((resolve, reject) => {
+        sass.render(opt, (err, res) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+                return;
+            }
+
+            console.log('rendering sass');
+            result.root = postcss.parse(res.css.toString(), {
+                from: result.opts.from,
+                map: {
+                    prev: mergeSourceMap(css.map.toJSON(), JSON.parse(css.map))
+                }
+            });
+
+            resolve();
+
+        });
     });
 });
